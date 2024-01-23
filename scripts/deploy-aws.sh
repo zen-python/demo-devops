cfd9ccc3484e71000a0713e8689b5872659cf89e
zen-python <33287337+zen-python@users.noreply.github.com>
2024-01-23 15:49:21 +0100

    feat: short sha-1 for tagging

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-TAG=$1
-AWS_ECR_ACCOUNT_URL=$2
-AWS_ECR_HELM_REPO_NAME=$3
-
+VERSION=$1
+CIRCLE_SHA1=$2
+AWS_ECR_ACCOUNT_URL=$3
+AWS_ECR_REPO_NAME=$4
+AWS_ECR_HELM_REPO_NAME=$5
+TAG=$VERSION-${CIRCLE_SHA1:0:7}
 echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
 
 export KUBECONFIG=$HOME/.kube/config
 
 result=$(eval helm ls | grep demo-devops-helm) 
 if [ $? -ne "0" ]; then 
-   helm install demo-devops-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
+   helm install demo-devops-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --set image.tag=$TAG --set image.repository=$AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO_NAME --version $TAG
 else 
-   helm upgrade demo-devops-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
+   helm upgrade demo-devops-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --set image.tag=$TAG --set image.repository=$AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO_NAME --version $TAG
 fi
